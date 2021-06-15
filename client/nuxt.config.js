e7fc9d8e982e4fefa72861e6d1b819c5a9505fc3
Shelomanov Dmitry <dmitryshelomanov@mail.ru>
2021-06-15 20:32:34 +0300

    chore: add publockpath

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -1,26 +1,28 @@
 module.exports = {
-    head: {
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        ],
-        link: [
-            { rel: 'stylesheet', href: '/css/main.css' },
-            { rel: 'stylesheet', href: '/css/font-awesome.min.css' },
-            { rel: 'stylesheet', href: 'https://unpkg.com/microtip/microtip.css' },
-            { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' }
-        ],
-        script: [
-
-        ]
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
+    link: [
+      { rel: "stylesheet", href: "/css/main.css" },
+      { rel: "stylesheet", href: "/css/font-awesome.min.css" },
+      { rel: "stylesheet", href: "https://unpkg.com/microtip/microtip.css" },
+      {
+        rel: "stylesheet",
+        href: "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css",
+      },
+    ],
+    script: [],
+  },
+  build: {
+    babel: {
+      presets: ["es2015", "stage-0", "stage-1", "stage-2", "stage-3"],
     },
+    vendor: ["axios", "localforage"],
     build: {
-        babel: {
-            presets: ["es2015", "stage-0", "stage-1", "stage-2", "stage-3"]
-        },
-        vendor: ['axios', 'localforage']
+      publicPath: "/market/",
     },
-    plugins: [
-        
-    ]
+  },
+  plugins: [],
 };
