1dedd485800a3b6596af0d7b5ab95e1b003f8296
Shelomanov Dmitry <dmitryshelomanov@mail.ru>
2021-06-15 15:33:07 +0300

    Updates

---
+++ _nuxt/5.nuxt.bundle.87086678e492f0e1c633.js
@@ -0,0 +1,1 @@
+webpackJsonp([5],{111:function(n,e){n.exports={render:function(){var n=this,e=n.$createElement;return(n._self._c||e)("nuxt")},staticRenderFns:[]}},68:function(n,e,t){var r=t(2)(null,t(111),null,null);n.exports=r.exports}});
